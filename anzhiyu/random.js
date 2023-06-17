7bddef10c287dc601623fe54f7a5e989f7ff3175
陌生人 <1957875100@qq.com>
2023-06-17 10:39:01 +0800

    Site updated: 2023-06-17 10:39:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/ae65.html","posts/3eeb.html","posts/99be.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/3eeb.html","posts/ae65.html","posts/99be.html","posts/923a.html","posts/cdc4.html","posts/f0ec.html","posts/d238.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
